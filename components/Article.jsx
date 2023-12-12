67f13c75c07c92379f494f539d0c55b22fd518d7
Jakerbp <jakerbparker@gmail.com>
2023-12-12 10:04:32 +0000

    completes view-single-article feature

--- components/Article.jsx
+++ components/Article.jsx
@@ -5,18 +5,32 @@ import axios from "axios";
 const Article = () => {
     const {article_id} = useParams()
     const [article, setArticle] = useState([])
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         axios.get(`https://newssite-zy4v.onrender.com/api/articles/${article_id}`).then((res) => {
           setArticle(res.data.article);
+          setLoading(false)
         });
       }, []);
+    const postedDate = new Date(article.created_at).toLocaleString();
+
+if(loading) return <h2 className="loading">LOADING...</h2> 
 
   return (
-    <>
+    <article className="article-section">
+        <div>
       <h2 className="article-title">{article.title}</h2>
-      <img src={article.article_img_url} />
-      <p>{article.body}</p>
-    </>
+      <img src={article.article_img_url} className="article-img"/>
+      <p className="article-body">{article.body}</p>
+      </div>
+      <div className="vote-comments-article">
+      <p className="date-posted">{postedDate}</p>
+      <p className="username">by {article.author}</p>
+        <p className="vote-count">{article.votes} Votes</p>
+        <p className="comment-count">{article.comment_count} Comments</p>
+      </div>
+
+    </article>
   );
 };
 
--- components/ArticleList.jsx
+++ components/ArticleList.jsx
@@ -5,12 +5,16 @@ import axios from "axios";
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     axios.get("https://newssite-zy4v.onrender.com/api/articles").then((res) => {
       setArticles(res.data.articles);
+      setLoading(false)
     });
   }, []);
+
+  if(loading) return <h2 className="loading">LOADING...</h2> 
 
   return (
     <div id="article-list-section">
