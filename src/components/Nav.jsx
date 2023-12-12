070dd367dd788678bd43db1b5d67bb9f93b8301b
Jakerbp <jakerbparker@gmail.com>
2023-12-12 17:03:15 +0000

    add userContext for postComment

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { UserContext } from "../src/contexts/User";
+import { UserContext } from "../contexts/User";
 
 const Nav = () => {
   const { user } = useContext(UserContext);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { UserProvider } from '../src/contexts/User.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    <UserProvider>
+        <App />
+    </UserProvider>
 
 )
