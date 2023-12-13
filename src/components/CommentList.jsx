dfb4f4cd165f61456910210f33ce9be29e14773c
Jakerbp <jakerbparker@gmail.com>
2023-12-13 15:23:03 +0000

    adds delete comment feature

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -21,7 +21,7 @@ const CommentList = ({ article_id }) => {
   return (
     <section className="comment-section">
       <PostComment article_id={article_id} setComments={setComments} setHideError={setHideError}/>
-      <h3 className="loading" hidden={comments}>
+      <h3 className="loading" hidden={comments.length!==0}>
         No comments yet...
       </h3>
       <h3 className="loading" hidden={hideError}>
--- src/utils.js
+++ src/utils.js
@@ -75,5 +75,16 @@ const sendComment = (article_id, user, commentMsg, setComments, setHideError, se
     });
 };
 
-export { fetchArticles, fetchArticle, fetchComments, sendComment, updateVote };
+const deleteComment = (comment_id, setOptimisticDelete) => {
+  return newsApi
+    .delete(`/comments/${comment_id}`)
+    .then(()=>{
+      setTimeout(()=>{setOptimisticDelete(true)}, 2000)
+    }).catch((err)=>{
+      setOptimisticDelete(false)
+      console.log(err)
+    })
+}
+
+export { fetchArticles, fetchArticle, fetchComments, sendComment, updateVote, deleteComment };
 
